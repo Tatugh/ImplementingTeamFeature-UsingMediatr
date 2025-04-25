13994302f95a7952e72d71fa609dbe8bd5663262
Tatugh <tatu.ruohoaho@hotmail.fi>
2025-04-25 23:00:56 +0300

    a

--- StudentEfCoreDemo.Domain/Entities/Team.cs
+++ StudentEfCoreDemo.Domain/Entities/Team.cs
@@ -9,8 +9,8 @@ namespace StudentEfCoreDemo.Domain.Entities
     public class Team
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string SportType { get; set; }
+        public string Name { get; set; } = null!;
+        public string SportType { get; set; } = null!;
         public DateTime FoundedDate { get; set; }
         public string HomeStadium { get; set; }
         public int MaxRosterSize { get; set; }
